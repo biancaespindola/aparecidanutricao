7b13113199fee5ea11a56ef4e97eb59cc125d5c5
Bianca Espíndola <32000570+BiancaEspindola@users.noreply.github.com>
2020-10-19 14:47:13 -0300

    adicionando novo paciente na tabela de pacientes

--- js/main.js
+++ js/main.js
@@ -8,12 +8,12 @@ var pacientes = document.querySelectorAll(".paciente");
 // var paciente03 = document.querySelector("#paciente03");
 
 //agora o instrutor vai usar for o que já da um alivio mental
-for(var i = 0; i < pacientes.length; i++){
+for (var i = 0; i < pacientes.length; i++) {
   console.log(i);
-  var paciente = pacientes[i] //ele vai modificar mais atributos atraves dessa propriedade por isso faz isso
+  var paciente = pacientes[i]; //ele vai modificar mais atributos atraves dessa propriedade por isso faz isso
 
   var tdPeso = paciente.querySelector(".info-peso"); //tenho certeza que há como melhorar essa parte
-  
+
   var pesoPaciente = tdPeso.textContent;
   var tdAltura = paciente.querySelector(".info-altura");
   var alturaPaciente = tdAltura.textContent;
@@ -26,38 +26,76 @@ for(var i = 0; i < pacientes.length; i++){
   var alturaValidacao = true;
 
   //verificação imc (de uma forma beeeem estranha ainda)
-  if(pesoPaciente < 0 || pesoPaciente >= 1000){
+  if (pesoPaciente < 0 || pesoPaciente >= 1000) {
     console.log("Peso Inválido!");
     pesoValidacao = false;
 
     tdImc.textContent = "Peso inválido!"; //parte da validação de imc
 
-    paciente.classList.add("paciente-invalido");   //modifica o estilo da linha para deixar mais evidente 
+    paciente.classList.add("paciente-invalido"); //modifica o estilo da linha para deixar mais evidente
   }
 
-  if(alturaPaciente <= 0 || alturaPaciente >= 3.00){
+  if (alturaPaciente <= 0 || alturaPaciente >= 3.0) {
     console.log("Altura inválida!");
     pesoValidacao = false;
 
     tdImc.textContent = "Altura inválida!"; //parte da validação de imc
 
-    paciente.classList.add("paciente-invalido");  //modifica o estilo da linha para deixar mais evidente
+    paciente.classList.add("paciente-invalido"); //modifica o estilo da linha para deixar mais evidente
   }
 
   //aqui o isntrutor usa as variaveis de validacao calcular o imc com valores validos
-  if(alturaValidacao && pesoValidacao){
+  if (alturaValidacao && pesoValidacao) {
     var imc = pesoPaciente / (alturaPaciente * alturaPaciente);
     tdImc.textContent = imc.toFixed(2); //trunca em duas casas decimais o imc
-    
   }
-
 }
 
-title.addEventListener("click", function(){
-  console.log("Olá eu fui clicado!");
+// title.addEventListener("click", function () {
+//   console.log("Olá eu fui clicado!");
+// });
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function (event) {
+  event.preventDefault();
+  // console.log("Oi, cliquei no botão");
+
+  var form = document.querySelector("#form-add");
+
+  var nomeNovoPaciente = form.nome.value;
+  var pesoNovoPaciente = form.peso.value;
+  var alturaNovoPaciente = form.altura.value;
+  var gorduraNovoPaciente = form.gordura.value;
+  // var imcNovoPaciente = 
+
+  // console.log(nomeNovoPaciente);
+  // console.log(pesoNovoPaciente);
+  // console.log(alturaNovoPaciente);
+  // console.log(gorduraNovoPaciente);
+
+  var novoPacienteTr = document.createElement("tr");
+  
+  var nomeNovoPacienteTd = document.createElement("td");
+  var pesoNovoPacienteTd = document.createElement("td");
+  var alturaNovoPacienteTd = document.createElement("td");
+  var gorduraNovoPacienteTd = document.createElement("td");
+  var imcNovoPacienteTd = document.createElement("td");
+
+  nomeNovoPacienteTd.textContent = nomeNovoPaciente;
+  pesoNovoPacienteTd.textContent = pesoNovoPaciente;
+  alturaNovoPacienteTd.textContent = alturaNovoPaciente;
+  gorduraNovoPacienteTd.textContent = gorduraNovoPaciente;
+
+  novoPacienteTr.appendChild(nomeNovoPacienteTd);
+  novoPacienteTr.appendChild(pesoNovoPacienteTd);
+  novoPacienteTr.appendChild(alturaNovoPacienteTd);
+  novoPacienteTr.appendChild(gorduraNovoPacienteTd);
+
+  console.log(novoPacienteTr);
+
+  var tabelaPacientes = document.querySelector("#tabela-pacientes");
+
+  tabelaPacientes.appendChild(novoPacienteTr);
+
 });
 
-var  botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click",function(){
-  console.log("Oi, cliquei no botão");
-});
