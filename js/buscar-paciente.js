4c1f894fac3176e0feada5acc6df99830c952bf7
Bianca Espíndola <32000570+BiancaEspindola@users.noreply.github.com>
2020-10-26 23:38:59 -0300

    tranforma o json em um array

--- js/buscar-paciente.js
+++ js/buscar-paciente.js
@@ -1,1 +1,22 @@
-console.log("buscar pacientes")
+var botaoAdicionar = document.querySelector("#buscar-pacientes");
+
+botaoAdicionar.addEventListener("click", function(){
+  console.log("Buscando pacientes...");
+
+  var xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "http://api-pacientes.herokuapp.com/pacientes");
+
+  xhr.addEventListener("load", function(){
+    // console.log(xhr.responseText);
+    var resposta = xhr.responseText;
+    console.log(resposta);
+    console.log(typeof resposta);
+
+    var pacientes = JSON.parse(resposta);
+    console.log(pacientes);
+    console.log(typeof pacientes);
+  });
+
+  xhr.send();
+});
